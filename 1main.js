ad627351ddebae34912db93bc78fb050591f91f6
TanyaRoot <kofeneformat@gmail.com>
2018-02-21 13:59:20 +0300

    2aaar

--- 1main.js
+++ 1main.js
@@ -1,1 +1,14 @@
+"use strict"
+
 alert("«Я – JavaScript!» А я - печенька");
+
+//5.Переменные
+var admin,
+    name;
+name = "Vasya";
+admin = name;
+console.log(admin);
+
+//6.Правильный выбор имени переменной
+var myPlanet = "Земля",
+    userName = "Петя";
